f8f435866132522c482258685996f2770cf53316
Tomasz Czernuszenko <tczernuszenko@gmail.com>
2018-06-27 09:48:05 +0100

    added marker (words frequent only in certain clusters) identification

--- src/nltk_classifier/__init__.py
+++ src/nltk_classifier/__init__.py
@@ -1,16 +1,16 @@
 from nltk_classifier.models.Clusters import Clusters
 from nltk_classifier.ml_techniques.NltkTraining import *
+__all__ = ["util", "models", "ml_techniques"]
 
 
 def main():
-    clusters = Clusters("/Users/tomaszczernuszenko/PycharmProjects/UCL-Classifier/data/excercise-new-clusters.json")
+    clusters = Clusters("/Users/tomaszczernuszenko/PycharmProjects/UCL-Classifier/data/excercise-subjective-objective.json")
     training_set = clusters.get_labeled_dataset()
     training = NltkTraining(training_set)
     training.train(0.9)
     i = clusters.identify_features_for_unknown("I don't like working out.")
     print(training.classifier.classify(i))
 
-__all__ = ["util", "models", "ml_techniques"]
 
 if __name__ == "__main__":
     main()
--- src/nltk_classifier/util/FeatureCreator.py
+++ src/nltk_classifier/util/FeatureCreator.py
@@ -8,11 +8,14 @@ class FeatureCreator:
         self.clusters = clusters
         self.expression = expression
         self.features = {}
+        self.tables = []
 
     def produce_features(self):
         self.feature_length()
         self.vocabulary_similarity_optimised()
         self.vocabulary_similarity_no_stopwords()
+        #self.objective_word_present()
+        self.identify_markers()
         self.expression.features = self.features
         return self.expression
 
@@ -21,17 +24,6 @@ class FeatureCreator:
         f.name = "Length of string"
         f.value = len(self.expression.text)
         self.features[f.name] = f
-
-    def vocabulary_similarity(self):
-        for cluster in self.clusters:
-            f = Feature()
-            f.name = "Single word similarity score for cluster" + cluster.name
-            f.value = 0
-            table = cluster.generate_vocab_table()
-            for word in self.expression.text.lower().replace(".", "").replace(",", "").split(' '):
-                if word in table:
-                    f.value += table[word]
-            self.features[f.name] = f
 
     def vocabulary_similarity_optimised(self):
         for cluster in self.clusters:
@@ -49,17 +41,6 @@ class FeatureCreator:
             self.features[f2.name] = f2
             self.features[f1.name] = f1
 
-    def weighted_vocabulary_similarity(self):
-        for cluster in self.clusters:
-            f = Feature()
-            f.name = "Weighted single word similarity score for cluster" + cluster.name
-            f.value = 0
-            table = cluster.generate_vocab_table()
-            for word in self.expression.text.lower().replace(".", "").replace(",", "").split(' '):
-                if word in table:
-                    f.value += table[word]*len(word)
-            self.features[f.name] = f
-
     def vocabulary_similarity_no_stopwords(self):
         for cluster in self.clusters:
             f = Feature()
@@ -70,7 +51,39 @@ class FeatureCreator:
                 if word in table:
                     f.value += table[word]*len(word)
             self.features[f.name] = f
+            self.tables.append(table)
 
+    def objective_word_present(self):
+        f = Feature()
+        f.name = "Key word score objective"
+        f.value = 0
+        with open("/Users/tomaszczernuszenko/PycharmProjects/UCL-Classifier/data/objective.txt", 'r') as input:
+            data = input.read()
+        total = 0.0
+        length = 0.0
+        for word in self.expression.text.lower().replace(".", "").replace(",", "").split(' '):
+            length += 1
+            if word in data:
+                total += 1
+        return total / length
+
+    def identify_markers(self):
+        lists = []
+        markers = []
+        for table in self.tables:
+            list_n = table.items()
+            lists.append(sorted(list_n, key=lambda tup: -tup[1]))
+        for list_n in lists:
+            for i in range(30):
+                if self. is_good_marker(list_n[i][0], list_n[i][1]):
+                    markers.append(list_n[i][0])
+        print(markers)
+
+    def is_good_marker(self, key, value):
+        for table in self.tables:
+            if key not in table or table[key]*2 < value:
+                return True
+        return False
     # TODO: create a hashtable for individual words without stop words (all + without given one)
     # TODO: create a hashtable for expressions (all + without given one)
     # TODO: create a hashtable for expressions without stop expressions (all + without given one)
